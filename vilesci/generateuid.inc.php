f260429fa8526955e82185bd130f3e38904433b6
oesi <oesi@technikum-wien.at>
2015-08-10 16:32:31 +0200

    Fixangestellte Mitarbeiter bekommen vorname.nachname UID Nebenamtliche bekommen fhb....

--- vilesci/generateuid.inc.php
+++ vilesci/generateuid.inc.php
@@ -29,12 +29,12 @@ function generateUID($stgkzl,$jahr, $stgtyp, $matrikelnummer)
 	$db = new basis_db();
 	$uid = 'fhb'.date('y');
 	$qry="
-	SELECT 
+	SELECT
 		lpad(id::text,4,'0') as freeid
-	FROM 
+	FROM
 		generate_series(1,9999) as s(id)
 	WHERE
-		lpad(id::text,4,'0') not in(SELECT substring(uid,6) as ids FROM public.tbl_benutzer WHERE uid like 'fhb15%') limit 1
+		lpad(id::text,4,'0') not in(SELECT substring(uid,6) as ids FROM public.tbl_benutzer WHERE uid like '".$uid."%') limit 1
 	";
 	$lastid=0;
 	if($result = $db->db_query($qry))
@@ -56,23 +56,54 @@ function generateUID($stgkzl,$jahr, $stgtyp, $matrikelnummer)
  * Gerneriert die Mitarbeiter UID
  * Format v.nachname max 20 Zeichen
  */
-function generateMitarbeiterUID($vorname, $nachname, $lektor)
+function generateMitarbeiterUID($vorname, $nachname, $lektor, $fixangestellt=true)
 {
 	$bn = new benutzer();
 	$uid='';
-	
-	// Nachname wird so lange verkuerzt bis eine eindeutige UID entsteht die noch nicht vergeben ist
-	for($nn=18;$nn!=0;$nn--)
-	{
-		$uid = mb_substr($vorname,0,1);
-		$uid .= '.'.mb_substr($nachname,0,$nn);
-				
-		$uid = mb_str_replace(' ','',$uid);
-		$uid = mb_str_replace('-','',$uid);
 
-		$uid = mb_strtolower($uid);
-		if(!$bn->uid_exists($uid))
-			return $uid;
+	// Fixangestellte bekommen eine UID mit vorname.nachname alle anderen eine fhb15...
+	if($fixangestellt)
+	{
+		// Nachname wird so lange verkuerzt bis eine eindeutige UID entsteht die noch nicht vergeben ist
+		for($nn=mb_strlen($nachname);$nn!=0;$nn--)
+		{
+			$uid .= mb_substr($vorname.'.'.mb_substr($nachname,0,$nn),0,20);
+
+			$uid = mb_str_replace(' ','',$uid);
+			$uid = mb_str_replace('-','',$uid);
+
+			$uid = mb_strtolower($uid);
+			if(!$bn->uid_exists($uid))
+				return $uid;
+		}
+	}
+	else
+	{
+		$db = new basis_db();
+		$uid = 'fhb'.date('y');
+		$qry="
+		SELECT
+			lpad(id::text,4,'0') as freeid
+		FROM
+			generate_series(1,9999) as s(id)
+		WHERE
+			lpad(id::text,4,'0') not in(SELECT substring(uid,6) as ids FROM public.tbl_benutzer WHERE uid like '".$uid."%') limit 1
+		";
+		$lastid=0;
+		if($result = $db->db_query($qry))
+		{
+			if($row = $db->db_fetch_object($result))
+			{
+				$freeid = $row->freeid;
+			}
+		}
+		else
+		{
+			die('Fehler beim Generieren der UID');
+		}
+		$uid.= sprintf('%04s',($freeid));
+		return $uid;
+
 	}
 	return false;
 }
