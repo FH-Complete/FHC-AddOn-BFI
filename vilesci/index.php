0f03bb97dfd13f54db5f5a392c78f431203c11bd
oesi <oesi@technikum-wien.at>
2015-08-25 13:44:32 +0200

    Link zur Ressourcenliste hinzugefügt

---
+++ vilesci/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" href="../../../skin/fhcomplete.css" type="text/css">
+	<link rel="stylesheet" href="../../../skin/vilesci.css" type="text/css">
+	<title>FH BFI Wien</title>
+</head>
+<body>
+<h1>FH BFI Wien</h1>
+<html>
+<body>
+	<a href="ressourcenliste.php">Ressourcenliste</a>
+</body>
+</html>
